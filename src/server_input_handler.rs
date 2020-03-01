5dcc2391dfa98a2399b8818ce533703f0a186a15
rooneyshuman <rooneyshuman@gmail.com>
2020-03-01 12:46:11 -0800

    Remove warnings on server_input_handler

--- src/server_input_handler.rs
+++ src/server_input_handler.rs
@@ -1,7 +1,7 @@
-use crate::proto::*;
 use crate::game_objects::*;
-use std::sync::{Arc, Mutex};
+use crate::proto::*;
 use std::collections::{HashMap, HashSet};
+use std::sync::{Arc, Mutex};
 
 // --------------- out of game --------------- //
 pub fn handle_out_of_game(
@@ -11,27 +11,26 @@ pub fn handle_out_of_game(
     active_nicks_mutex: &Arc<Mutex<HashSet<String>>>,
     id_nick_map_mutex: &Arc<Mutex<HashMap<u32, String>>>,
     client_msg: &Msg,
-    client_id: u32
+    client_id: u32,
 ) -> Msg {
     if cmd == Commands::InitSetup {
         return initial_setup(id_nick_map_mutex, client_id);
-    }
-    else if cmd == Commands::ListGames {
+    } else if cmd == Commands::ListGames {
         return list_active_games(game_list_mutex);
-    }
-    else if cmd == Commands::ListUsers {
+    } else if cmd == Commands::ListUsers {
         return list_active_users(active_nicks_mutex);
-    }
-    else if cmd == Commands::SetNick {
+    } else if cmd == Commands::SetNick {
         return set_nickname(active_nicks_mutex, id_nick_map_mutex, client_msg, client_id);
-    }
-    else if cmd == Commands::KillMe {
+    } else if cmd == Commands::KillMe {
         return client_disconnect(active_nicks_mutex, id_nick_map_mutex, client_id);
-    }
-    else if cmd == Commands::MakeNewGame {
-        return start_new_game(game_list_mutex, id_game_map_mutex, client_id, client_msg.data.clone());
-    }
-    else if cmd == Commands::JoinGame {
+    } else if cmd == Commands::MakeNewGame {
+        return start_new_game(
+            game_list_mutex,
+            id_game_map_mutex,
+            client_id,
+            client_msg.data.clone(),
+        );
+    } else if cmd == Commands::JoinGame {
         return join_game(game_list_mutex, id_game_map_mutex, client_id, client_msg);
     }
     Msg {
@@ -40,16 +39,13 @@ pub fn handle_out_of_game(
         command: Commands::Reply,
         game_status: GameStatus::NotInGame,
         data: String::new(),
-        game_state: GameState::new_empty()
+        game_state: GameState::new_empty(),
     }
 }
 
-
 // READ functions
-pub fn initial_setup(
-    id_nick_map_mutex: &Arc<Mutex<HashMap<u32, String>>>,
-    client_id: u32) -> Msg {
-    let mut id_nick_map_unlocked = id_nick_map_mutex.lock().unwrap();
+pub fn initial_setup(id_nick_map_mutex: &Arc<Mutex<HashMap<u32, String>>>, client_id: u32) -> Msg {
+    let id_nick_map_unlocked = id_nick_map_mutex.lock().unwrap();
     let nickname = id_nick_map_unlocked.get(&client_id).unwrap();
     Msg {
         status: Status::Ok,
@@ -57,7 +53,7 @@ pub fn initial_setup(
         command: Commands::Reply,
         game_status: GameStatus::NotInGame,
         data: format!("{}^{}", nickname, client_id),
-        game_state: GameState::new_empty()
+        game_state: GameState::new_empty(),
     }
 }
 
@@ -65,14 +61,16 @@ pub fn list_active_games(game_list_mutex: &Arc<Mutex<Vec<GameState>>>) -> Msg {
     let game_list_unlocked = game_list_mutex.lock().unwrap();
     let game_list_string: String = game_list_unlocked
         .iter()
-        .fold("Available Games: \n".to_string(), |acc, x| acc + &x.game_id.to_string() + ": " + &x.game_name + "\n");
+        .fold("Available Games: \n".to_string(), |acc, x| {
+            acc + &x.game_id.to_string() + ": " + &x.game_name + "\n"
+        });
     Msg {
         status: Status::Ok,
         headers: Headers::Response,
         command: Commands::Reply,
         game_status: GameStatus::NotInGame,
         data: game_list_string,
-        game_state: GameState::new_empty()
+        game_state: GameState::new_empty(),
     }
 }
 
@@ -87,19 +85,17 @@ pub fn list_active_users(active_nicks_mutex: &Arc<Mutex<HashSet<String>>>) -> Ms
         command: Commands::Reply,
         game_status: GameStatus::NotInGame,
         data: active_nicks_string,
-        game_state: GameState::new_empty()
+        game_state: GameState::new_empty(),
     }
 }
-
-// pub fn get_game_info() -> Msg {}
-
 
 // WRITE functions
 pub fn set_nickname(
     active_nicks_mutex: &Arc<Mutex<HashSet<String>>>,
     id_nick_map_mutex: &Arc<Mutex<HashMap<u32, String>>>,
     client_msg: &Msg,
-    client_id: u32) -> Msg {
+    client_id: u32,
+) -> Msg {
     let nickname = client_msg.data.clone();
     let mut active_nicks_unlocked = active_nicks_mutex.lock().unwrap();
     if active_nicks_unlocked.contains(&nickname) {
@@ -109,7 +105,7 @@ pub fn set_nickname(
             command: Commands::SetNick,
             game_status: GameStatus::NotInGame,
             data: "nickname already in use".to_string(),
-            game_state: GameState::new_empty()
+            game_state: GameState::new_empty(),
         };
     } else {
         let mut id_nick_map_unlocked = id_nick_map_mutex.lock().unwrap();
@@ -123,7 +119,7 @@ pub fn set_nickname(
             command: Commands::SetNick,
             game_status: GameStatus::NotInGame,
             data: format!("{}", nickname.clone()),
-            game_state: GameState::new_empty()
+            game_state: GameState::new_empty(),
         }
     }
 }
@@ -131,8 +127,8 @@ pub fn set_nickname(
 fn start_new_game(
     game_list_mutex: &Arc<Mutex<Vec<GameState>>>,
     id_game_map_mutex: &Arc<Mutex<HashMap<u32, u32>>>,
-    client_id: u32, mut
-    game_name: String
+    client_id: u32,
+    mut game_name: String,
 ) -> Msg {
     let mut game_list_unlocked = game_list_mutex.lock().unwrap();
     let mut id_game_map_unlocked = id_game_map_mutex.lock().unwrap();
@@ -140,11 +136,7 @@ fn start_new_game(
     if game_name.is_empty() {
         game_name = "New Game".to_string();
     }
-    let mut new_game = GameState::new(
-        client_id,
-        game_name.clone(),
-        game_id
-    );
+    let new_game = GameState::new(client_id, game_name.clone(), game_id);
     game_list_unlocked.push(new_game.clone());
     id_game_map_unlocked.insert(client_id, game_id);
     Msg {
@@ -153,7 +145,7 @@ fn start_new_game(
         command: Commands::MakeNewGame,
         game_status: GameStatus::InGame,
         data: "New Game".to_string(),
-        game_state: new_game
+        game_state: new_game,
     }
 }
 
@@ -161,7 +153,7 @@ fn join_game(
     game_list_mutex: &Arc<Mutex<Vec<GameState>>>,
     id_game_map_mutex: &Arc<Mutex<HashMap<u32, u32>>>,
     client_id: u32,
-    client_msg: &Msg
+    client_msg: &Msg,
 ) -> Msg {
     let mut game_list_unlocked = game_list_mutex.lock().unwrap();
     let mut id_game_map_unlocked = id_game_map_mutex.lock().unwrap();
@@ -175,14 +167,14 @@ fn join_game(
         command: Commands::JoinGame,
         game_status: GameStatus::InGame,
         data: format!("Joined Game {}", &game.game_name),
-        game_state: game.clone()
+        game_state: game.clone(),
     }
 }
 
 fn client_disconnect(
     active_nicks_mutex: &Arc<Mutex<HashSet<String>>>,
     id_nick_map_mutex: &Arc<Mutex<HashMap<u32, String>>>,
-    client_id: u32
+    client_id: u32,
 ) -> Msg {
     let mut active_nicks_unlocked = active_nicks_mutex.lock().unwrap();
     let mut id_nick_map_unlocked = id_nick_map_mutex.lock().unwrap();
@@ -194,10 +186,9 @@ fn client_disconnect(
         command: Commands::KillClient,
         game_status: GameStatus::NotInGame,
         data: format!("Nick {} successfully booted", nickname),
-        game_state: GameState::new_empty()
+        game_state: GameState::new_empty(),
     }
 }
-
 
 // --------------- in game --------------- //
 pub fn handle_in_game(
@@ -205,9 +196,9 @@ pub fn handle_in_game(
     game_list_mutex: &Arc<Mutex<Vec<GameState>>>,
     id_game_map_mutex: &Arc<Mutex<HashMap<u32, u32>>>,
     client_msg: &Msg,
-    client_id: u32
+    client_id: u32,
 ) -> Msg {
-    let mut id_game_map_unlocked = id_game_map_mutex.lock().unwrap();
+    let id_game_map_unlocked = id_game_map_mutex.lock().unwrap();
     let mut game_list_unlocked = game_list_mutex.lock().unwrap();
     let game_id = id_game_map_unlocked.get(&client_id).unwrap();
     let game: &mut GameState = &mut game_list_unlocked[*game_id as usize];
@@ -218,13 +209,12 @@ pub fn handle_in_game(
             command: Commands::GameIsOver,
             game_status: GameStatus::NotInGame,
             data: "Game Over".to_string(),
-            game_state: game.clone()
-        }
+            game_state: game.clone(),
+        };
     }
     if cmd == Commands::GetCurrentGamestate {
         return current_state(game);
-    }
-    else if cmd == Commands::MakeMove {
+    } else if cmd == Commands::MakeMove {
         return make_move(client_msg, game);
     }
     Msg {
@@ -233,7 +223,7 @@ pub fn handle_in_game(
         command: Commands::Reply,
         game_status: GameStatus::NotInGame,
         data: "Somethings wrong".to_string(),
-        game_state: game.clone()
+        game_state: game.clone(),
     }
 }
 
@@ -245,7 +235,7 @@ fn current_state(game: &GameState) -> Msg {
         command: Commands::Reply,
         game_status: GameStatus::InGame,
         data: "Current Game State".to_string(),
-        game_state: game.clone()
+        game_state: game.clone(),
     }
 }
 
@@ -259,10 +249,6 @@ pub fn make_move(client_msg: &Msg, game: &mut GameState) -> Msg {
         command: Commands::Reply,
         game_status: GameStatus::InGame,
         data: "Current Game State".to_string(),
-        game_state: game.clone()
+        game_state: game.clone(),
     }
 }
-
-pub fn leave_game() {} // TODO - implement?
-
-pub fn send_message() {} // TODO - implement?
