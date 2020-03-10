8bf93b2bdcc2df84ba3cdb32d0f49a0ed9378db1
loganballard <loganballard@gmail.com>
2020-03-09 18:36:27 -0700

    running some tests for connections, works pretty well, panics on the disconnect step, but well figure it out

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,4 @@ pub const SEPARATOR: char = '^';
 pub const SLOTS: usize = 7; // there are 6 playable slots and one goal slot
 pub const STARTING_STONES: u8 = 4;
 pub const BOARD_LENGTH: usize = SLOTS * 2;
+pub const SUPER_SECRET_PASSWORD: &str = "def62cce-c346-4f84-a4d9-1febd2470a2c";
